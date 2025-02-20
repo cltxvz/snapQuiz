6418ee9a0a94be1ca2a5b0db3463dab29e3e8e64
cltxvz <carlos.cardenas09@outlook.com>
2025-02-20 12:21:03 -0500

    Bug Fix

--- backend/app.py
+++ backend/app.py
@@ -192,5 +192,6 @@ def get_image():
     return jsonify({"error": "Could not fetch image"}), 500
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, port=5000)
+
     
